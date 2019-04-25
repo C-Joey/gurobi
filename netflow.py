1e1a26561d43336233bdd95a0bf1834cd3ea64aa
Joey-Chen <48274987+C-Joey@users.noreply.github.com>
2019-04-25 22:09:48 +0800

    s

--- netflow.py
+++ netflow.py
@@ -19,38 +19,38 @@ commodities = ['Pencils', 'Pens']
 nodes = ['Detroit', 'Denver', 'Boston', 'New York', 'Seattle']
 
 arcs, capacity = multidict({
-  ('Detroit', 'Boston'):   100,
-  ('Detroit', 'New York'):  80,
-  ('Detroit', 'Seattle'):  120,
-  ('Denver',  'Boston'):   120,
-  ('Denver',  'New York'): 120,
-  ('Denver',  'Seattle'):  120 })
+    ('Detroit', 'Boston'): 100,
+    ('Detroit', 'New York'): 80,
+    ('Detroit', 'Seattle'): 120,
+    ('Denver', 'Boston'): 120,
+    ('Denver', 'New York'): 120,
+    ('Denver', 'Seattle'): 120})
 
 cost = {
-  ('Pencils', 'Detroit', 'Boston'):   10,
-  ('Pencils', 'Detroit', 'New York'): 20,
-  ('Pencils', 'Detroit', 'Seattle'):  60,
-  ('Pencils', 'Denver',  'Boston'):   40,
-  ('Pencils', 'Denver',  'New York'): 40,
-  ('Pencils', 'Denver',  'Seattle'):  30,
-  ('Pens',    'Detroit', 'Boston'):   20,
-  ('Pens',    'Detroit', 'New York'): 20,
-  ('Pens',    'Detroit', 'Seattle'):  80,
-  ('Pens',    'Denver',  'Boston'):   60,
-  ('Pens',    'Denver',  'New York'): 70,
-  ('Pens',    'Denver',  'Seattle'):  30 }
+    ('Pencils', 'Detroit', 'Boston'): 10,
+    ('Pencils', 'Detroit', 'New York'): 20,
+    ('Pencils', 'Detroit', 'Seattle'): 60,
+    ('Pencils', 'Denver', 'Boston'): 40,
+    ('Pencils', 'Denver', 'New York'): 40,
+    ('Pencils', 'Denver', 'Seattle'): 30,
+    ('Pens', 'Detroit', 'Boston'): 20,
+    ('Pens', 'Detroit', 'New York'): 20,
+    ('Pens', 'Detroit', 'Seattle'): 80,
+    ('Pens', 'Denver', 'Boston'): 60,
+    ('Pens', 'Denver', 'New York'): 70,
+    ('Pens', 'Denver', 'Seattle'): 30}
 
 inflow = {
-  ('Pencils', 'Detroit'):   50,
-  ('Pencils', 'Denver'):    60,
-  ('Pencils', 'Boston'):   -50,
-  ('Pencils', 'New York'): -50,
-  ('Pencils', 'Seattle'):  -10,
-  ('Pens',    'Detroit'):   60,
-  ('Pens',    'Denver'):    40,
-  ('Pens',    'Boston'):   -40,
-  ('Pens',    'New York'): -30,
-  ('Pens',    'Seattle'):  -30 }
+    ('Pencils', 'Detroit'): 50,
+    ('Pencils', 'Denver'): 60,
+    ('Pencils', 'Boston'): -50,
+    ('Pencils', 'New York'): -50,
+    ('Pencils', 'Seattle'): -10,
+    ('Pens', 'Detroit'): 60,
+    ('Pens', 'Denver'): 40,
+    ('Pens', 'Boston'): -40,
+    ('Pens', 'New York'): -30,
+    ('Pens', 'Seattle'): -30}
 
 # Create optimization model
 m = Model('netflow')
@@ -60,7 +60,7 @@ flow = m.addVars(commodities, arcs, obj=cost, name="flow")
 
 # Arc capacity constraints
 m.addConstrs(
-    (flow.sum('*',i,j) <= capacity[i,j] for i,j in arcs), "cap")
+    (flow.sum('*', i, j) <= capacity[i, j] for i, j in arcs), "cap")
 
 # Equivalent version using Python looping
 # for i,j in arcs:
@@ -70,8 +70,8 @@ m.addConstrs(
 
 # Flow conservation constraints
 m.addConstrs(
-    (flow.sum(h,'*',j) + inflow[h,j] == flow.sum(h,j,'*')
-    for h in commodities for j in nodes), "node")
+    (flow.sum(h, '*', j) + inflow[h, j] == flow.sum(h, j, '*')
+     for h in commodities for j in nodes), "node")
 # Alternate version:
 # m.addConstrs(
 #   (quicksum(flow[h,i,j] for i,j in arcs.select('*',j)) + inflow[h,j] ==
@@ -86,6 +86,6 @@ if m.status == GRB.Status.OPTIMAL:
     solution = m.getAttr('x', flow)
     for h in commodities:
         print('\nOptimal flows for %s:' % h)
-        for i,j in arcs:
-            if solution[h,i,j] > 0:
-                print('%s -> %s: %g' % (i, j, solution[h,i,j]))
+        for i, j in arcs:
+            if solution[h, i, j] > 0:
+                print('%s -> %s: %g' % (i, j, solution[h, i, j]))
